d622153c9bead52c4f1d6ee51e93469803aeb71c
Afton <ronaldvernek1234@gmail.com>
2025-06-24 03:26:58 -0300

    hero, sobre e diferenciais

---
+++ casa-nova-starter-files-main/pages/erro.php
@@ -0,0 +1,14 @@
+<h1 class="text-center">
+    Erro 404
+</h1>
+<h2 class="text-center">
+    A página que está tentando acessar não existe ou foi removida!
+</h2>
+<p class="text-center">
+    <img src="imagens/404.avif" alt="404">
+</p>
+<p class="text-center">
+    <a href="home" class="btn btn-danger">
+        Página Inicial
+    </a>
+</p>
